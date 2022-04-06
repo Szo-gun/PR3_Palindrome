2908cdb60715d7478627df8514c0dbfd0d36d6fc
Piotr Cianciara <piotr.cianciara@comatec.fi>
2022-04-06 18:31:03 +0200

    Added coment to function testowanie

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 def testowanie(tested):
+    """
+    Print True/False depending on whether the word is a palindrome or not.
+    Arguments:
+    Tested - string
+    """
     reversed = tested[::-1]
     if tested == reversed:
         palindrome = 1
